a7858bf2e1fbb2a2410502c1bd15e64603852b4c
guanhongcheng <guanhongcheng@tcpjw.com>
2022-07-06 09:15:43 +0800

    指定端口

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,7 +2,6 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
-import org.example.boot.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
